c70dc7bcf6ba26700e4cb2fd6e31521b9a91306c
MounirSaghfary <129525946+MounirSaghfary@users.noreply.github.com>
2023-06-08 14:31:07 +0100

    first commit

--- pages/api/tsk.js
+++ pages/api/tsk.js
@@ -1,6 +1,6 @@
 const { sql, connect, disconnect } = require('../../utils/SQL');
-console.log('HELLO');
-async function handler(req, res) {
+
+ async function handler(req, res) {
     console.log('HELLO');
     try {
       console.log('Handler function called');
@@ -13,9 +13,9 @@ async function handler(req, res) {
       await disconnect();
       console.log('Disconnected from SQL Server');
       console.log('Sending API response');
+      res.status(200).json(result.recordset);
     } catch (error) {
       console.error('Error retrieving data from SQL Server:', error);
+      res.status(500).json({ error: 'Internal Server Error' });
     }
   }
-
-  handler();
